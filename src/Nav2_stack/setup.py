da52f5c1dc5bfa0ff2002201bc31e73bfec29c28
IDBirth <bilalbaslar@gmail.com>
2025-12-07 22:31:04 +0400

    made a nav_stack

---
+++ src/Nav2_stack/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'nav2_stack'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        # ament index registration
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        # package.xml
+        ('share/' + package_name, ['package.xml']),
+        # launch files
+        ('share/' + package_name + '/launch',
+         glob('launch/*.py')),
+        # config files
+        ('share/' + package_name + '/config',
+         glob('config/*.yaml')),
+        # maps (optional)
+        ('share/' + package_name + '/maps',
+         glob('maps/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Bilal',
+    maintainer_email='bilalbaslar@gmail.com',
+    description='Nav2 stack for my UGV',
+    license='TODO: License',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
